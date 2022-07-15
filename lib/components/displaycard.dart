58fd35badfa2e39316e7a82f6b8d4fe9fb7f0776
Frank van de Voorde <daemonaz@kymp.net>
2022-07-15 18:40:32 +0300

    Fix Fat Menu

--- lib/components/displaycard.dart
+++ lib/components/displaycard.dart
@@ -14,46 +14,49 @@ class DisplayCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 4.0,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-          child: SizedBox(
-            width: 250.0,
-            height: 275.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      header,
-                      style: Theme.of(context).textTheme.headline3,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 16.0),
-                    Text(
-                      subheader,
-                      style: Theme.of(context).textTheme.bodyText2,
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-                Container(
-                  width: 150.0,
-                  height: 150.0,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary,
-                      border: Border.all(
-                        color: Theme.of(context).colorScheme.secondary,
+    return InkWell(
+      child: Card(
+          elevation: 4.0,
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+            child: SizedBox(
+              width: 250.0,
+              height: 275.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        header,
+                        style: Theme.of(context).textTheme.headline3,
+                        textAlign: TextAlign.center,
                       ),
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(100))),
-                  child: icon,
-                )
-              ],
+                      const SizedBox(height: 16.0),
+                      Text(
+                        subheader,
+                        style: Theme.of(context).textTheme.bodyText2,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.secondary,
+                        border: Border.all(
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(100))),
+                    child: icon,
+                  )
+                ],
+              ),
             ),
-          ),
-        ));
+          )),
+    );
   }
 }
--- lib/components/fatmenu.dart
+++ lib/components/fatmenu.dart
@@ -23,163 +23,208 @@ class _FatMenuState extends State<FatMenu> {
           elevation: 5,
           child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Column(
-              children: const [
-                SizedBox(
-                  width: 280.0,
-                )
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 20.0),
-                Text("GAME MODES",
-                    style: Theme.of(context).textTheme.headline6),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Dungeons",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Scenarios",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Campaign",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                const SizedBox(height: 20.0),
-                Text("PLAY WITH OTHERS",
-                    style: Theme.of(context).textTheme.headline6),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Matchmaking",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Private Lobby",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Public Lobby",
-                        style: Theme.of(context).textTheme.bodyText1)),
-              ],
-            ),
-            Column(
-              children: const [
                 SizedBox(
-                  width: 176.0,
-                )
+                    width: 280.0,
+                    height: 318.0,
+                    child: Container(
+                        decoration: const BoxDecoration(
+                            color: Color(0xffF8F1F0),
+                            image: DecorationImage(
+                              image: AssetImage("assets/OdjzIV4.jpg"),
+                              fit: BoxFit.cover,
+                              opacity: 0.08,
+                            ))))
               ],
             ),
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const SizedBox(height: 20.0),
-              Text("THE CAMPAIGN",
-                  style: Theme.of(context).textTheme.headline6),
-              TextButton(
-                  style: buttonStyle,
-                  onPressed: null,
-                  child: Text("Create a lobby",
-                      style: Theme.of(context).textTheme.bodyText1)),
-              TextButton(
-                  style: buttonStyle,
-                  onPressed: null,
-                  child: Text("Join a lobby",
-                      style: Theme.of(context).textTheme.bodyText1)),
-              TextButton(
-                  style: buttonStyle,
-                  onPressed: null,
-                  child: Text("Persistant world state",
-                      style: Theme.of(context).textTheme.bodyText1)),
-              TextButton(
-                  style: buttonStyle,
-                  onPressed: null,
-                  child: Text("Expansions",
-                      style: Theme.of(context).textTheme.bodyText1)),
-              TextButton(
-                  style: buttonStyle,
-                  onPressed: null,
-                  child: Text("Resetting the campaign",
-                      style: Theme.of(context).textTheme.bodyText1)),
-              TextButton(
-                  style: buttonStyle,
-                  onPressed: null,
-                  child: Text("Leaving a campaign",
-                      style: Theme.of(context).textTheme.bodyText1)),
-              TextButton(
-                  style: buttonStyle,
-                  onPressed: null,
-                  child: Text("New game+",
-                      style: Theme.of(context).textTheme.bodyText1)),
-            ]),
-            Column(
-              children: const [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.symmetric(
+                      vertical: BorderSide(
+                          width: 1.0,
+                          color: const Color(0xff534340).withOpacity(0.15)))),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: SizedBox(
+                    width: 125.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 20.0),
+                        Text("GAME MODES",
+                            style: Theme.of(context).textTheme.headline6),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Dungeons",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Scenarios",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Campaign",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        const SizedBox(height: 20.0),
+                        Text("PLAY WITH OTHERS",
+                            style: Theme.of(context).textTheme.headline6),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Matchmaking",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Private Lobby",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Public Lobby",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                      ],
+                    ),
+                  ),
+                ),
+                Column(
+                  children: const [
+                    SizedBox(
+                      width: 176.0,
+                    )
+                  ],
+                ),
                 SizedBox(
-                  width: 176.0,
+                  width: 125.0,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 20.0),
+                        Text("THE CAMPAIGN",
+                            style: Theme.of(context).textTheme.headline6),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Create a lobby",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Join a lobby",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Persistant world state",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Expansions",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Resetting the campaign",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Leaving a campaign",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("New game+",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                      ]),
+                ),
+                Column(
+                  children: const [
+                    SizedBox(
+                      width: 176.0,
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: SizedBox(
+                    width: 200.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 20.0),
+                        Text("REWARDS",
+                            style: Theme.of(context).textTheme.headline6),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Achievements",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Titles",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Avatars",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Badges",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        const SizedBox(height: 20.0),
+                        Text("GAME EDITOR",
+                            style: Theme.of(context).textTheme.headline6),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Create",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Share",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Browse",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        TextButton(
+                            style: buttonStyle,
+                            onPressed: null,
+                            child: Text("Play",
+                                style: Theme.of(context).textTheme.bodyText1)),
+                        const SizedBox(height: 20.0),
+                      ],
+                    ),
+                  ),
                 )
-              ],
+              ]),
             ),
             Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 20.0),
-                Text("REWARDS", style: Theme.of(context).textTheme.headline6),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Achievements",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Titles",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Avatars",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Badges",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                const SizedBox(height: 20.0),
-                Text("GAME EDITOR",
-                    style: Theme.of(context).textTheme.headline6),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Create",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Share",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Browse",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                TextButton(
-                    style: buttonStyle,
-                    onPressed: null,
-                    child: Text("Play",
-                        style: Theme.of(context).textTheme.bodyText1)),
-                const SizedBox(height: 20.0),
-              ],
-            ),
-            Column(
-              children: const [
                 SizedBox(
-                  width: 1500.0,
-                )
+                    width: 1500.0,
+                    height: 318,
+                    child: Container(
+                        decoration: const BoxDecoration(
+                            color: Color(0xffF8F1F0),
+                            image: DecorationImage(
+                              image: AssetImage("assets/OdjzIV4.jpg"),
+                              fit: BoxFit.contain,
+                              alignment: Alignment.centerLeft,
+                              opacity: 0.08,
+                            ))))
               ],
             )
           ]),
